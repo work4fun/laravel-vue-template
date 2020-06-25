2f0fc23794b2fbcb9e87039147429e50f3036bc7
Albert <albert_p@foxmail.com>
2020-06-26 01:47:21 +0800

    chore: Remove redundant front-end code

--- laravel/resources/js/bootstrap.js
+++ laravel/resources/js/bootstrap.js
@@ -8,6 +8,5 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
-window.chart = require('chart.js');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
